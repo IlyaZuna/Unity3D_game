733fe142f667d354cdac6cf09c94156214dbfd07
IlyaZuna <illya.z2002@gmail.com>
2023-05-19 18:33:58 +0300

    Прописал логику подбора предметов и загрузки сцены при выигрыше

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -54,12 +54,19 @@ public class GameBehavior : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.Box(new Rect(20, 20, 1500, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect(100, 20, 150, 25), "Player Health:" + _playerHP);
 
-        GUI.Box(new Rect(20, 50, 1500, 25), "Items Collected: " + _itemsCollected);
+        GUI.Box(new Rect(100, 50, 150, 25), "Items Collected: " + _itemsCollected);
 
         GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
 
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
+            {
+
+            }
+        }
     }
 
 
